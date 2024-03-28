38d4b8b0a129ef2df9666dac6a00307bb7700b4e
Muhammad Salman Shahid <muhammad-salman.shahid@grenoble-inp.fr>
2024-03-28 11:44:30 +0100

    another shell file

--- ibm_coursera/linux/conditional_scripting.sh
+++ ibm_coursera/linux/conditional_scripting.sh
@@ -16,3 +16,26 @@ else
    echo "Your response must be either 'y' or 'n'."
    echo "Please re-run the script to try again."
 fi
+
+# 2nd example
+echo -n "Enter an integer: "
+read n1
+echo -n "Enter another integer: "
+read n2
+
+sum=$(($n1+$n2))
+product=$(($n1*$n2))
+
+echo "The sum of $n1 and $n2 is $sum"
+echo "The product of $n1 and $n2 is $product."
+
+if [ $sum -lt $product ]
+then
+   echo "The sum is less than the product."
+elif [[ $sum == $product ]]
+then
+   echo "The sum is equal to the product."
+elif [ $sum -gt $product ]
+then
+   echo "The sum is greater than the product."
+fi
